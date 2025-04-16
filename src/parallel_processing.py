74e03efe70b96a4fd5df57d48426c399b7fc31a5
soft7197 <122602093+soft7197@users.noreply.github.com>
2025-04-16 16:50:28 +0900

    Update parallel_processing.py

--- src/parallel_processing.py
+++ src/parallel_processing.py
@@ -9,12 +9,12 @@ def get_available_gpus():
 
 def process_file_with_gpu(file, gpu_id):
     """ Assigns a bug processing task to a specific GPU. """
-    file_path = os.path.join("/home/selab/Desktop/dlt-conf/java/selab/extracted_methods", file)
+    file_path = os.path.join("----", file)
     print(f"Processing {file} on GPU {gpu_id}...")
     subprocess.run(["python3", "embed_and_index.py", file_path, str(gpu_id)])
 
 def main():
-    dir_path = "/home/selab/Desktop/dlt-conf/java/selab/extracted_methods"
+    dir_path = "----"
     files = os.listdir(dir_path)
 
     gpus = get_available_gpus()
